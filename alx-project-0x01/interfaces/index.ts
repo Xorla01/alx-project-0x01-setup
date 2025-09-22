b6e695acc24d3e416bf570c9db1deb6578d31f10
Richmond Cowther <richmondcowther@gmail.com>
2025-09-22 09:39:35 +0000

    Add files via upload

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -44,3 +44,38 @@ export interface PostModalProps {
   onClose: () => void;
   onSubmit: (post: PostData) => void;
 }
+
+export interface UserModalProps {
+    onClose: () => void;
+    onSubmit: (user: {
+    name: string;
+    username: string;
+    email: string;
+    phone: string;
+    website: string;
+  }) => void;
+}
+
+export interface UserDataProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
